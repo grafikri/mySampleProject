4af15febadbcf11498f6e1da10084b2d11084278
Serhan Coşkun <grafikri@gmail.com>
2021-03-17 13:00:51 +0300

    feat: change layout logic to gain more flexible structure

---
+++ src/views/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header @handleClickSignOut="handleClickSignOut" />
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<script>
+
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'MainLayout',
+  components: {
+    Header,
+    Footer,
+  },
+  methods: {
+    handleClickSignOut() {
+      console.log('logout');
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
