927bec34ddb57d54526bfe1f2e8e9b8e8cd0126c
Serhan Coşkun <grafikri@gmail.com>
2021-03-17 18:50:01 +0300

    feat: implement modal and login form
    provide user logged in via store methods

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -5,34 +5,61 @@
       :user-name="user.name"
       :user-email="user.email"
       @handleClickSignOut="handleClickSignOut"
+      @handleClickLogin="handleClickLogin"
     />
+
     <slot />
     <Footer />
+
+    <b-modal
+      ref="login-modal"
+      title="Login"
+      hide-footer
+    >
+      <LoginForm @submit="loginSubmit" />
+    </b-modal>
   </div>
 </template>
 
 <script>
+
+import LoginForm from '@/components/LoginForm.vue';
 
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
 import { mapMutations, mapState } from 'vuex';
 
+import { BModal } from 'bootstrap-vue';
+
 export default {
   name: 'MainLayout',
   components: {
     Header,
     Footer,
+    LoginForm,
+    BModal,
   },
   computed: {
     ...mapState(['user']),
   },
   methods: {
     ...mapMutations('user', [
+      'login',
       'logout',
     ]),
     handleClickSignOut() {
       this.logout();
+    },
+    handleClickLogin() {
+      this.$refs['login-modal'].show();
+    },
+    /**
+     * @param {{name: String, email: String, password: String}} payload
+     */
+    loginSubmit(payload) {
+      this.login({ name: payload.name, email: payload.email });
+      this.$refs['login-modal'].hide();
     },
   },
 };
