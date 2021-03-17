2d1b65afa136f6704cd1c4f0b75fde7209ec8ea3
Serhan Coşkun <grafikri@gmail.com>
2021-03-18 00:26:25 +0300

    feat: define langs files

---
+++ src/locales/en.js
@@ -0,0 +1,42 @@
+export default {
+  app: {
+    turkish: 'Turkish',
+    english: 'English',
+    lang: 'Lang',
+    login: 'Login',
+  },
+  form: {
+    yourName: 'Your name',
+    enterName: 'Enter name',
+    emailAddress: 'Email address',
+    enterEmail: 'Enter email',
+    yourPhoneNumber: 'Your phone number',
+    enterCellPhoneNumber: 'Enter cell phone e.g. - 5551234567',
+    country: 'Country',
+    yourThoughts: 'Your thoughts',
+    enterSomething: 'Enter something...',
+    password: 'Password',
+    enterPassword: 'Enter password',
+    countryDesc: 'You must select a country from list',
+  },
+  button: {
+    submit: 'Submit',
+    login: 'Login',
+    logout: 'Logout',
+    contact: 'Contact',
+    home: 'Home',
+  },
+  pages: {
+    contact: {
+      title: 'Contact',
+      desc: 'This is CONTACT page description',
+    },
+    home: {
+      title: 'Home',
+      desc: 'This is HOME page description',
+    },
+  },
+  footer: {
+    text: "I'm the footer",
+  },
+};
---
+++ src/locales/tr.js
@@ -0,0 +1,42 @@
+export default {
+  app: {
+    turkish: 'Türkçe',
+    english: 'İngilizce',
+    lang: 'Dil',
+    login: 'Giriş',
+  },
+  form: {
+    yourName: 'Adınız',
+    enterName: 'İsim girin',
+    emailAddress: 'E-posta adresiniz',
+    enterEmail: 'E-posta girin',
+    yourPhoneNumber: 'Telefon numaranız',
+    enterCellPhoneNumber: 'Telefon orn. - 5551234567',
+    country: 'Şehir',
+    yourThoughts: 'Düşünceleriniz',
+    enterSomething: 'Birşeyler yazın...',
+    password: 'Şifre',
+    enterPassword: 'Şifre girin',
+    countryDesc: 'Listeden en az bir şehir şeçmelisiniz',
+  },
+  button: {
+    submit: 'Gönder',
+    login: 'Giriş yap',
+    logout: 'Çıkış yap',
+    contact: 'İletişim',
+    home: 'Ana Sayfa',
+  },
+  pages: {
+    contact: {
+      title: 'İletişim',
+      desc: 'Bu metin İLETİŞİM sayfasının açıklamasıdır',
+    },
+    home: {
+      title: 'Ana Sayfa',
+      desc: 'Bu metin ANA SAYFA sayfasının açıklamasıdır',
+    },
+  },
+  footer: {
+    text: "Ben footer'ın ordayım",
+  },
+};
